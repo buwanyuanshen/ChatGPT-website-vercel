74a41fa1f6dc137dda6465ad0f620f20d97fc3d5
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-03-09 18:15:28 +0800

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -1,1 +1,59 @@
+# -*- coding: utf-8 -*-
+# SECRET_KEY（flask项目密钥,不用管,也用不到）
+SECRET_KEY = "asghnjadfbtywyn"
 
+# openAi api key（用于免费gpt3.5模型）
+API_KEYS = [
+    "sk-",
+    "sk-",
+]
+# openAi 代理 api（用于免费gpt3.5模型）
+API_URL = "https://api.openai-proxy.com"
+
+
+# openAi api key1（用于gpt4模型，对应app.py中第1个授权码）
+API_KEYS1 = [
+     "sk-",
+    "sk-",
+]
+# openAi 代理 api1（用于gpt4模型，对应app.py中第1个授权码)
+API_URL1 = "https://api.openai-proxy.com"
+
+
+# openAi api key2（用于gpt4模型，对应app.py中第2个授权码）
+API_KEYS2 = [
+    "sk-",
+    "sk-",
+]
+# openAi 代理 api2（用于gpt4模型，对应app.py中第2个授权码)
+API_URL2 = "https://api.openai-proxy.com"
+
+
+# openAi api key3（用于gpt4模型，对应app.py中第3个授权码）
+API_KEYS3 = [
+    "sk-",
+    "sk-",
+]
+# openAi 代理 api3（用于gpt4模型，对应app.py中第3个授权码)
+API_URL3 = "https://api.openai-proxy.com"
+
+
+# openAi api key4（用于gpt4模型，对应app.py中第4个授权码）
+API_KEYS4 = [
+    "sk-",
+    "sk-",
+]
+# openAi 代理 api4（用于gpt4模型，对应app.py中第4个授权码)
+API_URL4 = "https://api.openai-proxy.com"
+
+
+# openAi api key5（用于gpt4模型，对应app.py中第5个授权码）
+API_KEYS5 = [
+    "sk-",
+    "sk-",
+]
+# openAi 代理 api5（用于gpt4模型，对应app.py中第5个授权码)
+API_URL5 = "https://api.openai-proxy.com"
+
+
+#  ............
