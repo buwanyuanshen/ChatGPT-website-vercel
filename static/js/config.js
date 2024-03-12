85b1e3e37138fdb250594cfe0231f25eb82de30f
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-03-13 00:20:24 +0800

    Delete static/js/config.js

--- static/js/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
-    url: "https://api.openai-proxy.com/v1/chat/completions",  // gpt3.5代理接口
-    apiKey: "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"  // openAi Base64 编码 apiKey
-};  
