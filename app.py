94572652f355cb1a2ac7989dc6f7659e6b7e3a2f
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2025-04-17 19:26:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -348,6 +348,8 @@ def chat():
                     "model": model,
                     "max_tokens": int(max_tokens),
                     "n": 1,
+                                    "stream": True,
+
             }
         elif "o1" in model and "all" not in model:
             api_url += "/v1/chat/completions"
@@ -358,6 +360,8 @@ def chat():
                     "temperature": 1,
                     "top_p": 1,
                     "n": 1,
+                                    "stream": True,
+
             }
         elif "o3" in model and "all" not in model:
             api_url += "/v1/chat/completions"
@@ -368,6 +372,8 @@ def chat():
                     "temperature": 1,
                     "top_p": 1,
                     "n": 1,
+                                    "stream": True,
+
             }
         elif "o4" in model and "all" not in model:
             api_url += "/v1/chat/completions"
@@ -378,6 +384,8 @@ def chat():
                     "temperature": 1,
                     "top_p": 1,
                     "n": 1,
+                                    "stream": True,
+
             }
         else:
             # 对于其他模型，使用原有 api_url
@@ -389,6 +397,8 @@ def chat():
                 "temperature": float(temperature),
                 "top_p": 1,
                 "n": 1,
+                                    "stream": True,
+
             }
 
     elif "grok-3-mini" in model:
@@ -400,6 +410,8 @@ def chat():
                     "temperature": float(temperature),
                     "top_p": 1,
                     "n": 1,
+                                    "stream": True,
+
             }
     elif "grok-2-image" in model:
             api_url += "/v1/chat/completions"
@@ -415,6 +427,8 @@ def chat():
                     "model": model,
                     "max_tokens": int(max_tokens),
                     "n": 1,
+                                "stream": True,
+
             }    
     else:
         # 对于其他模型，使用原有 api_url
